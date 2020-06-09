a0d5ae246c1fd4235ea80dace3777083ed008303
priyankagarg <priyankagarg@aquevix.com>
2020-06-09 16:51:23 +0530

    Added module files.

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='aquevix_redis_session_store',
+    version='1.0',
+    packages=[''],
+    url='https://github.com/Aquevix/aquevix_redis_session_store',
+    license='AGPL-3',
+    author='Aquevix (original: smile)',
+    author_email='',
+    description='Customized module for internal users timeout with redis session'
+)
